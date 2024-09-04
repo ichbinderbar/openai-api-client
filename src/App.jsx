38fa7fb22367d80237ad8824e1b2c23733bf8f68
ichbinderbar <idrovel@gmail.com>
2024-09-05 00:45:45 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.scss";
+
+const apiUrl = "https://openai-experimental-server-eff701d4fdb7.herokuapp.com/";
+
+function App() {
+  const [prompt, setPrompt] = useState("");
+  const [response, setResponse] = useState("");
+
+  const handleInputChange = (event) => {
+    setPrompt(event.target.value);
+  };
+
+  const getResponse = async () => {
+    try {
+      const result = await axios.post(
+        "https://openai-experimental-server-eff701d4fdb7.herokuapp.com/api/get-response",
+        { prompt }
+      );
+      setResponse(result.data.choices[0].message.content);
+    } catch (error) {
+      console.error(
+        "Error fetching response:",
+        error.response ? error.response.data : error.message
+      );
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={prompt}
+        onChange={handleInputChange}
+        placeholder="Enter your prompt here"
+      />
+      <button onClick={getResponse}>Get Response</button>
+      <div>{response}</div>
+    </div>
+  );
+}
+
+export default App;
